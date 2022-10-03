86b7ca853774d709d55ac1e05ad82506d9f3cf72
bielViccari <gabriel.viccari20@gmail.com>
2022-10-03 15:59:13 -0300

    add firebase to backend-blog

--- src/server.js
+++ src/server.js
@@ -1,17 +1,45 @@
+import fs from 'fs';
+import admin from 'firebase-admin';
 import express, { application, json } from 'express';
 import { db, connectToDb } from './db.js' //create your db file and connect to your mongo server.
 
+//lendo as informações de auth do firebase
+const credentials = JSON.parse(
+    fs.readFileSync('../credential.json')
+)
+
+//iniciando o firebase no backend com as credenciais do firebase
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+})
 
 const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+app.use( async (req, res, next) => {
+    const { authtoken } = req.headers
+    
+    if(authtoken) {
+       try {
+          const user = await admin.auth().verifyIdToken(authtoken)
+       } catch (e) {
+        res.sendStatus(400)
+       }
+    }
+
+    next();
+})
+
 app.get('/api/articles/:name', async (req,res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name })
 
     if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid)
         res.json(article)
     } else {
         res.sendStatus(404)
